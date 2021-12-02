92290065abd2299ae2bf8977fe8b2afd07f114d5
Douglas-CRVG <douglas.07.nascimento@gmail.com>
2021-12-02 01:38:18 -0300

    central componentization completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Header from "./Header.js";
+import Footer from "./footer/footer.js";
+import Header from "./header/Header.js";
 import Main from "./main/main.js";
+
 export default function App() {
     return(
         <div>
             <Header />
             <Main />
-            
+            <Footer />            
         </div>
     );
 }
---
+++ src/footer/footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer>
+      <ion-icon name="home"></ion-icon>
+      <ion-icon name="search-outline"></ion-icon>
+      <ion-icon name="add-circle-outline"></ion-icon>
+      <ion-icon name="heart-outline"></ion-icon>
+      <ion-icon name="person-outline"></ion-icon>
+    </footer>
+  );
+}
